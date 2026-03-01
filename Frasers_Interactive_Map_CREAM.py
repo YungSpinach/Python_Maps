723f647e1c2c7222708ec703537e5865e5072f8c
JPT <jamieparkstaylor@gmail.com>
2026-03-01 14:38:44 +0000

    more icons

--- Frasers_Interactive_Map_CREAM.py
+++ Frasers_Interactive_Map_CREAM.py
@@ -210,23 +210,23 @@ for _, row in df_out.iterrows():
         # Formatting logic based on "Format"
         if fmt == "Transvision Screen":
             # Extra styling for large blue square using HTML DivIcon
-            html = f"""<div style="background-color: grey; width: 12px; height: 12px;"></div>"""
+            html = f"""<div style="background-color: darkgreen; width: 12px; height: 12px;"></div>"""
             icon = folium.DivIcon(html=html)
             folium.Marker([lat, lon], popup=row['Location'], icon=icon).add_to(layer_outdoor)
             
         elif fmt == "Motion Waterloo":
             # Extra styling for large dark blue square using HTML DivIcon
-            html = f"""<div style="background-color: grey; width: 12px; height: 12px;"></div>"""
+            html = f"""<div style="background-color: darkgreen; width: 12px; height: 12px;"></div>"""
             icon = folium.DivIcon(html=html)
             folium.Marker([lat, lon], popup=row['Location'], icon=icon).add_to(layer_outdoor)
             
         elif fmt == "Rail Digital 6 Sheet":
-            folium.Circle([lat, lon], popup=row['Location'], radius=5, 
-                                color='green', fill_color='green', fill_opacity=0.7).add_to(layer_outdoor)
+            folium.Circle([lat, lon], popup=row['Location'], radius=10, 
+                                color='lightgreen', fill_color='lightgreen', fill_opacity=0.7).add_to(layer_outdoor)
             
         elif fmt == "Road Digital 6 Sheet":
-            folium.Circle([lat, lon], popup=row['Location'], radius=5, 
-                                color='darkgreen', fill_color='green', fill_opacity=0.7).add_to(layer_outdoor)
+            folium.Circle([lat, lon], popup=row['Location'], radius=10, 
+                                color='lightgreen', fill_color='lightgreen', fill_opacity=0.7).add_to(layer_outdoor)
 
 layer_outdoor.add_to(m)
 
